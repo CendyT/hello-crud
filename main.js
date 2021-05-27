e1abd68cfaa9e72de7f6813657ac6e3f292dfb74
CendyT <cendy.rychellit@gmail.com>
2021-05-27 20:20:56 -0300

    alterado main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+const usuarioFernando = {
+    id: 1,
+    nome: 'Fernando',
+    idade: 27
+};
+
+const usuarios = [usuarioFernando];
+
+app.get('/usuarios', (req, res) => {
+    res.json(usuarios);
+});
+
+app.listen(port, () => {
+    console.log('Server is up and running on port ${port}');
+});
