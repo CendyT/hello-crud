6e501398fa0e2438c2e06ef8716c279b8f3dd7af
CendyT <cendy.rychellit@gmail.com>
2021-05-27 21:11:51 -0300

    adicionando host ao main.js

--- main.js
+++ main.js
@@ -16,6 +16,23 @@ app.get('/usuarios', (req, res) => {
     res.json(usuarios);
 });
 
+app.post('/usuarios', (req, res) => {
+    const { nome, idade } = req.body;
+    const id = usuarios.length;
+
+    const novoUsuario = {
+        id,
+        nome,
+        idade
+    };
+
+    usuarios.push(novoUsuario);
+    res.json(novoUsuario);
+
+
+});
+
+
 app.listen(port, () => {
     console.log('Server is up and running on port ${port}');
 });
